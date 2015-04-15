3b21064f407a70fa5695c97262293cc2f6766e2c
re4lfl0w <re4lfl0w@gmail.com>
2015-04-16 04:51:59 +0900

    Delete polls/

--- polls/urls.py
+++ polls/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, include, url
 from polls import views
 
 urlpatterns = patterns('',
-    url(r'^polls/$', views.index, name='index'),
-    url(r'^polls/(?P<question_id>\d+)/$', views.detail, name='detail'),
-    url(r'^polls/(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-    url(r'^polls/(?P<question_id>\d+)/results/$', views.vote, name='results'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^(?P<question_id>\d+)/results/$', views.vote, name='results'),
 )
 
 
