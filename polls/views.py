705ce6e3ac50bb9747189cd482ae6d3a56fc21ee
re4lfl0w <re4lfl0w@gmail.com>
2015-04-16 04:34:27 +0900

    Add views.vote

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,8 @@
+# -*- coding: utf-8
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from polls.models import Question
+from polls.models import Question, Choice
 
 
 def index(request):
@@ -11,3 +14,21 @@ def index(request):
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
+
+
+def vote(request, question_id):
+    p = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        # 설문 투표 폼을 다시 보여준다.
+        return render(request, 'polls/detail.html', {
+            'question': p,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # POST 데이터를 정상적으로 처리하였으면,
+        # 항상 HttpResponseRedirect를 반환하여 리다이렉션 처리함
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
