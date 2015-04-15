130411c4dc035f26b90cb15213cc433c1f5d9b06
re4lfl0w <re4lfl0w@gmail.com>
2015-04-16 04:12:54 +0900

    Add views.detail and detail.html

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from polls.models import Question
 
 
@@ -6,3 +6,8 @@ def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
