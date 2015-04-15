096aff73d8b5b9f5dc26fef1b569bc973609894d
re4lfl0w <re4lfl0w@gmail.com>
2015-04-16 05:01:09 +0900

    Modify votes.vote -> votes.results

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-    url(r'^(?P<question_id>\d+)/results/$', views.vote, name='results'),
+    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
 )
 
 
