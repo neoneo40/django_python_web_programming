bdc85a2fd9535520be499059d7e39559c7700e33
re4lfl0w <re4lfl0w@gmail.com>
2015-04-16 03:02:43 +0900

    Add polls URLconf

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,10 +3,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from polls import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^polls/$', views.index, name='index'),
+    url(r'^polls/(?P<question_id>\d+)/$', views.detail, name='detail'),
+    url(r'^polls/(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
+    url(r'^polls/(?P<question_id>\d+)/results/$', views.vote, name='results'),
     url(r'^admin/', include(admin.site.urls)),
 )
