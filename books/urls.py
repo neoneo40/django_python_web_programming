b825c34f4c39fa8d4d995bf5af006df4f6358976
re4lfl0w <re4lfl0w@gmail.com>
2015-04-17 02:33:24 +0900

    Add urlConf

---
+++ books/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, url
+from books import views
+
+urlpatterns = patterns('',
+    url(r'^$', 'views.BooksModelView.as_view()', name='index'),
+
+    url(r'^book/$', views.BookList.as_view(), name='book_list'),
+    url(r'^author/$', views.AuthorList.as_view(), name='author_list'),
+    url(r'^publisher/$', views.PublisherList.as_view(), name='publisher_list'),
+
+    url(r'^book/(?P<pk>\d+)/$', views.BookDetail.as_view(), name='book_detail'),
+    url(r'^author/(?P<pk>\d+)/$', views.AuthorDetail.as_view(), name='author_detail'),
+    url(r'^publisher/(?P<pk>\d+)/$', views.PublisherDetail.as_view(), name='publisher_detail'),
+)
--- mysite/urls.py
+++ mysite/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^polls/', include('polls.urls', namespace='polls')),
+    url(r'^books/', include('books.urls', namespace='books')),
     url(r'^admin/', include(admin.site.urls)),
 )
