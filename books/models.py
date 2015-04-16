dfbb2f2746f2dfbf0213603a85aedde47dd17676
re4lfl0w <re4lfl0w@gmail.com>
2015-04-17 02:23:20 +0900

    Modify Publisher class to the top of file. Because Book class call Publisher class

--- books/models.py
+++ books/models.py
@@ -1,6 +1,15 @@
 from django.db import models
 
 
+class Publisher(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    website = models.URLField()
+
+    def __unicode__(self):
+        return self.name
+
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField('Author')
@@ -15,15 +24,6 @@ class Author(models.Model):
     salutation = models.CharField(max_length=100)
     name = models.CharField(max_length=50)
     email = models.EmailField()
-
-    def __unicode__(self):
-        return self.name
-
-
-class Publisher(models.Model):
-    name = models.CharField(max_length=50)
-    address = models.CharField(max_length=100)
-    website = models.URLField()
 
     def __unicode__(self):
         return self.name
