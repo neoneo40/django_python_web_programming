a4399d19e2b3a3404e1da78e71086ee751f32321
re4lfl0w <re4lfl0w@gmail.com>
2015-04-17 03:53:25 +0900

    Modify urlconf. <pk>, viewname

--- polls/urls.py
+++ polls/urls.py
@@ -1,12 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    url(r'^$', views.IndexView.as_view(), name='index'), # /polls/
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'), #/polls/5/
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'), # /polls/5/vote/
 )
-
-
-
