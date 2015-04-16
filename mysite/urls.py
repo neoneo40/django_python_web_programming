0c74fd68970415dfe6b8b641bbb5758c008467ce
re4lfl0w <re4lfl0w@gmail.com>
2015-04-17 03:32:50 +0900

    Add URLconf

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from mysite import views
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.HomeView.as_view(), name='home'), # Add
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^books/', include('books.urls', namespace='books')),
     url(r'^admin/', include(admin.site.urls)),
